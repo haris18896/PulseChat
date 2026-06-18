2b4e7c8a73819d773fb8e21b07017b876cf27393
haris.ahmed <haris.ahmed@reporteq.com>
2026-06-18 12:35:54 +0500

    prisma client generated outside the src directory

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '../../generated/Prisma/client';
 
 @Injectable()
 export class PrismaService
